6e924bf2c0b7db5496e51fbfbdd14acac55defb8
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-05-10 16:09:10 +0100

    Added new showcase site

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+#app
+  navigation
+  router-view
+</template>
+
+<script>
+import Navigation from '@/components/Navigation'
+
+export default {
+  name: 'app',
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style lang="stylus">
+
+@import '~stylus/app'
+
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+</style>
